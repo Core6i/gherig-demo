9852aa5ec463bec9641d93ada3fcf70beea948c2
Kwabena Amponsah <kwabena@Kwabenas-MacBook-Pro.local>
2026-05-02 14:14:32 +0000

    enable cors

--- ncris/src/index.js
+++ ncris/src/index.js
@@ -261,6 +261,15 @@ async function main() {
 
   // HTTP server
   const server = http.createServer((rawReq, rawRes) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+if (req.method === "OPTIONS") {
+  res.writeHead(204);
+  res.end();
+  return;
+}
     const res = wrapResponse(rawRes);
     router.handle(rawReq, res).catch(err => {
       logger.error({ event: 'router.unhandled', error: err.message, stack: err.stack });
